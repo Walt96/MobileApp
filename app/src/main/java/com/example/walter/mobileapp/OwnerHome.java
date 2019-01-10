4b83086168412ff235b552d31327003ad23a024d
Walter <waltercarvelli96@gmail.com>
2019-01-10 23:05:42 +0100

    changed graphic

--- app/src/main/java/com/example/walter/mobileapp/OwnerHome.java
+++ app/src/main/java/com/example/walter/mobileapp/OwnerHome.java
@@ -80,17 +80,7 @@ public class OwnerHome extends AppCompatActivity
         datePattern = Pattern.compile(DATEREGEX);
 
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
+        
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
